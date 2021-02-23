7402e94257943f161d2fe84cfc4dfd4f39a2b094
Nylo F. Pinto <mastercoy@gmail.com>
2021-02-23 13:42:15 -0300

    Final do dia 2.

--- rocketpay/config/config.exs
+++ rocketpay/config/config.exs
@@ -8,20 +8,39 @@
 use Mix.Config
 
 config :rocketpay,
-  ecto_repos: [Rocketpay.Repo]
+       ecto_repos: [Rocketpay.Repo]
 
 # Configures the endpoint
-config :rocketpay, RocketpayWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "hSY+cFMKJ39cAaUhel5GEKQd81a8Qi60wshS6osZnGE1MlS/dcNg27zPcG2khYhV",
-  render_errors: [view: RocketpayWeb.ErrorView, accepts: ~w(json), layout: false],
-  pubsub_server: Rocketpay.PubSub,
-  live_view: [signing_salt: "V9fMYcMy"]
+config :rocketpay,
+       RocketpayWeb.Endpoint,
+       url: [
+         host: "localhost"
+       ],
+       secret_key_base: "hSY+cFMKJ39cAaUhel5GEKQd81a8Qi60wshS6osZnGE1MlS/dcNg27zPcG2khYhV",
+       render_errors: [
+         view: RocketpayWeb.ErrorView,
+         accepts: ~w(json),
+         layout: false
+       ],
+       pubsub_server: Rocketpay.PubSub,
+       live_view: [
+         signing_salt: "V9fMYcMy"
+       ]
+
+config :rocketpay,
+       Rocketpay.Repo,
+       migration_primary_key: [
+         type: :binary_id
+       ],
+       migration_foreign_key: [
+         type: :binary_id
+       ]
 
 # Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+config :logger,
+       :console,
+       format: "$time $metadata[$level] $message\n",
+       metadata: [:request_id]
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
--- rocketpay/lib/rocketpay_web/router.ex
+++ rocketpay/lib/rocketpay_web/router.ex
@@ -8,6 +8,7 @@ defmodule RocketpayWeb.Router do
   scope "/api", RocketpayWeb do
     pipe_through :api
     get "/:filename", WelcomeController, :index
+    post "/users", UserController, :create
   end
 
   # Enables LiveDashboard only for development
